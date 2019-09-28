05d14138e160fe4615b86c18070f7f2463fb7957
michael joh <michaeljoh99@gmail.com>
2019-09-27 21:37:50 -0400

    Skeletal Layout of all homepage info

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,31 @@
-import React from "react"
-import './HomePage.css'
+import React from "react";
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <div>
-      <h1>Globe Chat</h1>
-      <h2>Made by Team 20</h2>
+      <div>
+        <btn> Log In </btn>
+        <btn> Sign Up </btn>
+      </div>
+      <div>
+        <h1> Globe Chat </h1>
+        <h3> Team20 </h3>
+      </div>
+      <div>
+        Global Nomads Group harnesses the power of peer learning, intergroup
+        contact theory, and storytelling in order to prepare young people to
+        thrive in the 21st Century and lead positive change in their
+        communities. Guided by academic research and deep empathy for young
+        people’s interests and experiences, GNG’s curriculum leverages
+        storytelling and peer learning as the mechanism for helping youth
+        develop the knowledge, skills, and attitudes they need to thrive in the
+        21st Century. Through sharing stories and building human connections,
+        youth are inspired to learn and lead positive change in their
+        communities, both local and global, and in ways both large and small.
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
