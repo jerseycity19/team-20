8607e14a97c416b7797bf7ffcc9d68dccd30af95
Sarah Jang <sjang1@andrew.cmu.edu>
2019-09-28 02:57:27 -0400

    Testing twilio

--- src/components/user_profile_page.js
+++ src/components/user_profile_page.js
@@ -6,31 +6,16 @@ function UserProfilePage() {
   return (
     <Container>
         <Row className='text-center additional-bottom-padding'>
-          <Col><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="new" width = {300} height = {300}  style={{opacity: 0.5}}/></Col>
+          <Col><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="new" width = {200} height = {200}  style={{opacity: 0.5}}/></Col>
           <Col className="text-center text-md-left">
-          <Col><h1>Sarah Jang</h1><Card>
-                  <Card.Header>
-                    <Nav variant="tabs" defaultActiveKey="#first">
-                      <Nav.Item>
-                        <Nav.Link href="#first">Active</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link href="#disabled" disabled>
-                          Disabled
-                        </Nav.Link>
-                      </Nav.Item>
-                    </Nav>
-                  </Card.Header>
-                </Card>
-              </Col>
+               <h1>Sarah Jang</h1>
+               <Col>United States</Col>
+               <Col>Korean American</Col>
           </Col>        
         </Row>
         <Row className='additional-bottom-padding pb-lg-5 pt-lg-5 pl-lg-5 pr-lg-5 row text-center'>
-          <Col><Card className="bg-dark text-black">
-              <Card.Img src="https://s3.amazonaws.com/media.eremedia.com/wp-content/uploads/2018/04/06122011/story.jpeg" alt="Card image" width = {500} height = {300} style = {{opacity: .5}}/>
+          <Col><Card className="bg-dark text-white">
+              <Card.Img src="https://s3-media4.fl.yelpcdn.com/bphoto/WEbWusCKWWmx0MI58JV-AQ/ls.jpg" alt="Card image" width = {500} height = {300} style = {{opacity: .5}}/>
               <Card.ImgOverlay>
                 <Card.Title>Today</Card.Title>
                 <Card.Text>
@@ -40,7 +25,7 @@ function UserProfilePage() {
               </Card.ImgOverlay>
             </Card>
           </Col>
-          <Col><Card className="bg-dark text-black">
+          <Col><Card className="bg-dark text-white">
               <Card.Img src="https://img.officetimeline.com/website/Content/website/online-general/slider/graphic-built-with-online-timeline-and-Gantt-chart-app.png" alt="Card image" width = {500} height = {300} style = {{opacity: .5}}/>
               <Card.ImgOverlay>
                 <Card.Title>Her Timeline</Card.Title>
@@ -52,8 +37,8 @@ function UserProfilePage() {
             </Card>
           </Col>
         </Row>
-        <Row className='additional-bottom-padding pb-lg-5 pt-lg-5 pl-lg-7 pr-lg-7 row text-center'>
-          <Col><Card className="bg-dark text-black" width = {500} height = {300}>
+        <Row className='additional-bottom-padding row text-center'>
+          <Col sm="12" md={{ size: 6, offset: 4 }}><Card className="bg-dark text-white" width = {500} height = {300}>
               <Card.Img src="http://www.cmu.edu/assets/images/site/meta-image-cmu.jpg" alt="Card image"  style = {{opacity: .5}}/>
               <Card.ImgOverlay>
                 <Card.Title>Her World</Card.Title>
@@ -64,6 +49,7 @@ function UserProfilePage() {
               </Card.ImgOverlay>
             </Card>
           </Col>
+          <Col></Col>
         </Row>
     </Container>
   );
