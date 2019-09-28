186afc8cd03c5f633060da4ff3e1eb6bad66abfa
Megan Shen <meganshen@Megans-MacBook-Pro.local>
2019-09-28 02:50:39 -0400

    testing twilio

--- src/components/logged_in_pg.js
+++ src/components/logged_in_pg.js
@@ -4,15 +4,15 @@ function LoggedInLand() {
      return (
       <Container>
         <Row>
-        <Col ><img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png" alt="united states" height={70} width={140}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/c/ca.gif" alt="canada" height={70} width={140}/> </Col>
-        <Col> <img src="https://cdn.britannica.com/40/5340-004-FA6D2EE9/Flag-Afghanistan.jpg" alt="afghanistan" height={70} width={140}/> </Col>
+        <Col ><img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png" alt="united states" height={70} width={140} onClick = {'./chatroom.js'}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/c/ca.gif" alt="canada" height={70} width={140} onClick = {'./chatroom.js'}/> </Col>
+        <Col> <img src="https://cdn.britannica.com/40/5340-004-FA6D2EE9/Flag-Afghanistan.jpg" alt="afghanistan" height={70} width={140} onClick = {'./chatroom.js'}/> </Col>
         </Row>
         
         <Row>
-         <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/SP-flag.gif" alt="spain" height={70} width={140}/> </Col>
-        <Col> <img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/PL-flag.gif" alt="poland" height={70} width={140}/> </Col>
-        <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/FR-flag.gif" alt="france" height={70} width={140}/> </Col>
+         <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/SP-flag.gif" alt="spain" height={70} width={140} onClick = {'./chatroom.js'}/> </Col>
+        <Col> <img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/PL-flag.gif" alt="poland" height={70} width={140} onClick = {'./chatroom.js'}/> </Col>
+        <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/FR-flag.gif" alt="france" height={70} width={140} onClick = {'./chatroom.js'}/> </Col>
         </Row>
         
         <Row>
