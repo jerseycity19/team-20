7aab4cd5f271ed383d2329612277c973aa2e5868
Megan Shen <meganshen@Megans-MacBook-Pro.local>
2019-09-28 07:17:51 -0400

    finished flag cards

--- src/components/logged_in_pg.js
+++ src/components/logged_in_pg.js
@@ -1,51 +1,176 @@
 import React from 'react';
 //import ChatRoom from './ChatRoom';
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 import { Container, Row, Col } from 'reactstrap';
+import {Link } from "react-router-dom";
+
 function LoggedInLand() {
 
      return (
       <Container>
 
         <Row>
-        <Col ><Card className="US">
-              <Card.Img src="https://www.crwflags.com/fotw/images/u/us.gif" alt="Card image" width = {500} height = {300} style = {{opacity: .5}}/>
-              <Card.ImgOverlay>
-                <Card.Title>Today</Card.Title>
-                <Card.Text>
-                  United States
-                </Card.Text>
-                <Card.Text>9/28/19</Card.Text>
-              </Card.ImgOverlay>
-            </Card> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/c/ca.gif" alt="canada" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        <Col> <img src="https://cdn.britannica.com/40/5340-004-FA6D2EE9/Flag-Afghanistan.jpg" alt="afghanistan" height={140} width={280}/> </Col>
-        </Row>
-        
+        <Col ><Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/u/us.gif" width={140} height={200} />
+    <Card.Body>
+    <Card.Title>United States</Card.Title>
+    <Card.Text>
+      3 Americans Online
+    </Card.Text>
+    <Link to="/chat">
+    <Button variant="primary">Go to America</Button>
+    </Link>
+    </Card.Body>
+    </Card>
+     </Col>
+        <Col>
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/c/ca.gif" width={140} height={200} style={{opacity: .3}}/>
+    <Card.Body>
+    <Card.Title style={{opacity: .3}}>Canada</Card.Title>
+    <Card.Text style={{opacity: .3}}>
+      0 Canadians Online
+    </Card.Text>
+    <Button disabled = {true} variant="primary" style={{opacity: .3}}>Go to Canada</Button>
+    </Card.Body>
+    </Card>
+      </Col>
+        <Col> 
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/r/ro.gif" width={140} height={200} style={{opacity: .3}}/>
+    <Card.Body>
+    <Card.Title style={{opacity: .3}}>Romania</Card.Title>
+    <Card.Text style={{opacity: .3}}>
+      0 Romanians Online
+    </Card.Text>
+    <Button disabled = {true} variant="primary" style={{opacity: .3}}>Go to Romania</Button>
+    </Card.Body>
+    </Card>
+         </Col>
+         </Row>
+        <br></br>
+
+
+
         <Row>
-         <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/SP-flag.gif" alt="spain"height={140} width={280} /> </Col>
-        <Col> <img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/PL-flag.gif" alt="poland" style = {{opacity: .2}} height={140} width={280} /> </Col>
-        <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/FR-flag.gif" alt="france" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        </Row>
-        
-        <Row>
-          <Col><img src="https://www.crwflags.com/fotw/images/m/mx.gif" alt="mexico" height={140} width={280}/> </Col>
-          <Col><img src="https://www.crwflags.com/fotw/images/s/sv.gif" alt="el salvador" height={140} width={280}/> </Col>
-          <Col><img src="https://www.crwflags.com/fotw/images/a/ar.gif" alt="argentina" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        </Row>
-        
-        <Row>
-        <Col><img src="https://www.crwflags.com/fotw/images/c/cn.gif" alt="china" height={140} width={280}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/i/in.gif" alt="india" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/r/ru.gif" alt="russia" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        </Row>
-        
-        <Row>
-        <Col><img src="https://www.crwflags.com/fotw/images/e/eg.gif" alt="egypt" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/n/ng.gif" alt="niger" style = {{opacity: .2}} height={140} width={280}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/a/au.gif" alt="australia" height={140} width={280}/> </Col>
-        </Row>
-        
+        <Col> 
+        <Card style={{ width: '22rem' }}>
+  <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/a/af.gif"width={140} height={200} />
+  <Card.Body>
+    <Card.Title>Afghanistan</Card.Title>
+    <Card.Text>
+      4 Afghanistans Online
+    </Card.Text>
+    <Button variant="primary">Go to Afghanistan</Button>
+  </Card.Body>
+</Card>
+</Col>
+        <Col> 
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/z/za.gif" width={140} height={200} style={{opacity: .3}}/>
+    <Card.Body>
+    <Card.Title style={{opacity: .3}}>South Africa</Card.Title>
+    <Card.Text style={{opacity: .3}}>
+      0 South Africans Online
+    </Card.Text>
+    <Button disabled = {true} variant="primary" style={{opacity: .3}}>Go to South Africa</Button>
+    </Card.Body>
+    </Card>
+</Col>
+<Col>
+<Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/m/mx.gif"width={140} height={200} />
+    <Card.Body>
+    <Card.Title >Mexico</Card.Title>
+    <Card.Text>
+      4 Mexicans Online
+    </Card.Text>
+    <Button variant="primary">Go to Mexico</Button>
+    </Card.Body>
+    </Card>
+</Col>
+</Row>
+<br></br>
+<Row>
+        <Col ><Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/c/cn.gif" width={140} height={200}/>
+    <Card.Body>
+    <Card.Title>China</Card.Title>
+    <Card.Text>
+      5 Chinese Online
+    </Card.Text>
+    <Button variant="primary" /*onClick = {}*/>Go to China</Button>
+    </Card.Body>
+    </Card>
+     </Col>
+        <Col>
+
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/t/tr.gif" width={140} height={200}/>
+    <Card.Body>
+    <Card.Title >Turkey</Card.Title>
+    <Card.Text>
+      5 Turkic People Online
+    </Card.Text>
+    <Button variant="primary">Go to Turkey</Button>
+    </Card.Body>
+    </Card>
+      </Col>
+        <Col> 
+
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/r/ro.gif" width={140} height={200} style={{opacity: .3}}/>
+    <Card.Body>
+    <Card.Title style={{opacity: .3}}>Romania</Card.Title>
+    <Card.Text style={{opacity: .3}}>
+      0 Romanians Online
+    </Card.Text>
+    <Button disabled = {true} variant="primary" style={{opacity: .3}}>Go to Romania</Button>
+    </Card.Body>
+    </Card>
+         </Col>
+         </Row>
+         <br></br>
+         <Row>
+        <Col ><Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/u/ua.gif" width={140} height={200}/>
+    <Card.Body>
+    <Card.Title>United States</Card.Title>
+    <Card.Text>
+      3 Ukrainians Online
+    </Card.Text>
+    <Button variant="primary">Go to Ukraine</Button>
+    </Card.Body>
+    </Card>
+     </Col>
+        <Col>
+
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/t/th.gif" width={140} height={200} style={{opacity: .3}}/>
+    <Card.Body>
+    <Card.Title style={{opacity: .3}}>Thailand</Card.Title>
+    <Card.Text style={{opacity: .3}}>
+      0 Thai people Online
+    </Card.Text>
+    <Button disabled = {true} variant="primary" style={{opacity: .3}}>Go to Thailand</Button>
+    </Card.Body>
+    </Card>
+      </Col>
+        <Col> 
+
+        <Card style={{ width: '22rem' }}>
+    <Card.Img variant="top" src="https://www.crwflags.com/fotw/images/s/se.gif"width={140} height={200} />
+    <Card.Body>
+    <Card.Title>Sweden</Card.Title>
+    <Card.Text >
+      2 Swedish people Online
+    </Card.Text>
+    <Button variant="primary" >Go to Sweden</Button>
+    </Card.Body>
+    </Card>
+         </Col>
+         </Row>
+         <br></br>
       </Container>
     );
 }
