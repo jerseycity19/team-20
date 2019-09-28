3d84e31dda748706fd7377cd84f52a8dc00f5f9c
michael joh <michaeljoh99@gmail.com>
2019-09-28 07:05:06 -0400

    minor tweaks

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -22,34 +22,6 @@ function LoginPage() {
         </div>
       </div>
     </div>
-    // <div className="wrapper">
-    //   <NavBarComp />
-    //   <Card style={{ width: "18rem" }}>
-    //     <Card.Body>
-    //       <Form className="loginForm">
-    //         <Form.Group controlId="formBasicEmail">
-    //           <Form.Label>Email address</Form.Label>
-    //           <Form.Control type="email" placeholder="Enter email" />
-    //         </Form.Group>
-
-    //         <Form.Group controlId="formBasicPassword">
-    //           <Form.Label>Password</Form.Label>
-    //           <Form.Control type="password" placeholder="Password" />
-    //         </Form.Group>
-    //         <div className="buttonStuff">
-    //           <Button variant="primary" type="submit" href="/loggedin">
-    //             Log In
-    //           </Button>
-    //         </div>
-    //         <div className="buttonStuff">
-    //           <Link to={"/signup"} className="signup">
-    //             Not a User?
-    //           </Link>
-    //         </div>
-    //       </Form>
-    //     </Card.Body>
-    //   </Card>
-    // </div>
   );
 }
 
