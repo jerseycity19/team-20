be78be3afb616567b8ae19d48bd11e0951f01494
Aswin Nair <aswintorch@gmail.com>
2019-09-28 02:12:23 -0400

    Env

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,38 +1,43 @@
 import React from "react";
-import NavBarComp from "./NavBarComp";
+import NavBarComp from "./NavBarComp"
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import "./LoginPage.css";
-import { Alert } from "reactstrap";
+import { Alert } from 'reactstrap';
+
 
 function LoginPage() {
   return (
     <div>
-      <div>
-        <Alert color="success">Signed up successfully!</Alert>
-      </div>
-      <div className="centered">
+    <div>
+    <Alert color="success">
+        Signed up successfully!
+      </Alert>
+    </div>
+    <div className="centered">
+    
         <NavBarComp />
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Form className="loginForm">
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
+    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+     <Form className="loginForm">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Button variant="primary" type="submit" href="/loggedin">
-                Log In
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </div>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit" href="/loggedin">
+          Log In
+        </Button>
+      </Form>
+  </Card.Body>
+</Card>
+     
+    </div>
     </div>
   );
 }
