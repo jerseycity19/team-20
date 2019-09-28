fc88cc87b9a4fdd4939a4478c30c2114a5969899
michael joh <michaeljoh99@gmail.com>
2019-09-28 01:41:01 -0400

    restructuring login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,37 +1,37 @@
 import React from "react";
-import NavBarComp from "./NavBarComp"
+import NavBarComp from "./NavBarComp";
 import { Form } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import { Card } from "react-bootstrap";
 import "./LoginPage.css";
 
-
 function LoginPage() {
   return (
-    
-    <div className="centered">
-  
-        <NavBarComp />
-    <Card style={{ width: '18rem' }}>
-  <Card.Body>
-     <Form className="loginForm">
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit" href="/loggedin">
-          Log In
-        </Button>
-      </Form>
-  </Card.Body>
-</Card>
-     
+    <div className="wrapper">
+    <div className="form-wrapper">
+        <h1>Login</h1>
+            <div className="email">
+                <label htmlFor="email">Email</label>
+                <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                />
+            </div>
+            <div className="password">
+                <label htmlFor="password">Password</label>
+                <input
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                />
+            </div>
+            <div className="createAccount">
+                <button type="submit">Login</button>
+                <small>Dont Have an Account?</small>
+            </div>
     </div>
+</div>
   );
 }
 
