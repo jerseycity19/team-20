5cdff2bb86c9de3fa12c4572bb1a4051fc871813
Megan Shen <meganshen@Megans-MacBook-Pro.local>
2019-09-28 05:07:11 -0400

    opacity flags

--- src/components/logged_in_pg.js
+++ src/components/logged_in_pg.js
@@ -1,36 +1,38 @@
 import React from 'react';
+import ChatRoom from './ChatRoom';
 import { Container, Row, Col } from 'reactstrap';
 function LoggedInLand() {
+
      return (
       <Container>
         <Row>
-        <Col ><img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png" alt="united states" height={70} width={140}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/c/ca.gif" alt="canada" height={70} width={140}/> </Col>
-        <Col> <img src="https://cdn.britannica.com/40/5340-004-FA6D2EE9/Flag-Afghanistan.jpg" alt="afghanistan" height={70} width={140}/> </Col>
+        <Col ><img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png" alt="united states" height={140} width={280}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/c/ca.gif" alt="canada" style = {{opacity: .2}} height={140} width={280}/> </Col>
+        <Col> <img src="https://cdn.britannica.com/40/5340-004-FA6D2EE9/Flag-Afghanistan.jpg" alt="afghanistan" height={140} width={280}/> </Col>
         </Row>
         
         <Row>
-         <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/SP-flag.gif" alt="spain" height={70} width={140}/> </Col>
-        <Col> <img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/PL-flag.gif" alt="poland" height={70} width={140}/> </Col>
-        <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/FR-flag.gif" alt="france" height={70} width={140}/> </Col>
+         <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/SP-flag.gif" alt="spain"height={140} width={280} /> </Col>
+        <Col> <img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/PL-flag.gif" alt="poland" style = {{opacity: .2}} height={140} width={280} /> </Col>
+        <Col><img src="https://www.cia.gov/library/publications/the-world-factbook/attachments/flags/FR-flag.gif" alt="france" style = {{opacity: .2}} height={140} width={280}/> </Col>
         </Row>
         
         <Row>
-          <Col><img src="https://www.crwflags.com/fotw/images/m/mx.gif" alt="mexico" height={70} width={140}/> </Col>
-          <Col><img src="https://www.crwflags.com/fotw/images/s/sv.gif" alt="el salvador" height={70} width={140}/> </Col>
-          <Col><img src="https://www.crwflags.com/fotw/images/a/ar.gif" alt="argentina" height={70} width={140}/> </Col>
+          <Col><img src="https://www.crwflags.com/fotw/images/m/mx.gif" alt="mexico" height={140} width={280}/> </Col>
+          <Col><img src="https://www.crwflags.com/fotw/images/s/sv.gif" alt="el salvador" height={140} width={280}/> </Col>
+          <Col><img src="https://www.crwflags.com/fotw/images/a/ar.gif" alt="argentina" style = {{opacity: .2}} height={140} width={280}/> </Col>
         </Row>
         
         <Row>
-        <Col><img src="https://www.crwflags.com/fotw/images/c/cn.gif" alt="china" height={70} width={140}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/i/in.gif" alt="india" height={70} width={140}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/r/ru.gif" alt="russia" height={70} width={140}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/c/cn.gif" alt="china" height={140} width={280}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/i/in.gif" alt="india" style = {{opacity: .2}} height={140} width={280}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/r/ru.gif" alt="russia" style = {{opacity: .2}} height={140} width={280}/> </Col>
         </Row>
         
         <Row>
-        <Col><img src="https://www.crwflags.com/fotw/images/e/eg.gif" alt="egypt" height={70} width={140}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/n/ng.gif" alt="niger" height={70} width={140}/> </Col>
-        <Col><img src="https://www.crwflags.com/fotw/images/a/au.gif" alt="australia" height={70} width={140}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/e/eg.gif" alt="egypt" style = {{opacity: .2}} height={140} width={280}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/n/ng.gif" alt="niger" style = {{opacity: .2}} height={140} width={280}/> </Col>
+        <Col><img src="https://www.crwflags.com/fotw/images/a/au.gif" alt="australia" height={140} width={280}/> </Col>
         </Row>
         
       </Container>
