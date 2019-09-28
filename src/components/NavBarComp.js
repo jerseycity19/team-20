7610072628c43b471c00e9fe7b033cb16fb1a106
Aswin Nair <aswintorch@gmail.com>
2019-09-27 23:34:41 -0400

    Did more navbar

--- src/components/NavBarComp.js
+++ src/components/NavBarComp.js
@@ -2,24 +2,11 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/Navbar'
-import withRouter from 'react-router'
+
 function NavBarComp() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">GlobeChat</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <NavDropdown title="Profile" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Log Out</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div></div>
   );
 }
 
-export default withRouter(NavBarComp)
+export default NavBarComp
