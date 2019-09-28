1f084d9503644a98d5d907e60799fbcc8baf50e4
Megan Shen <meganshen@Megans-MacBook-Pro.local>
2019-09-27 22:26:12 -0400

    fhdhk

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
+import LoggedInLand from './components/logged_in_pg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={HomePage} />
           <Route exact path="/login" render={LoginPage} />
+          <Route exact path="/LoggedInLand" render={LoggedInLand}/>
         </Switch>
       </Router>
       </header>
